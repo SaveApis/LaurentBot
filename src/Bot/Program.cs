70602562834fe1332e60c6bbd929d557e8e99956
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-27 15:16:31 +0100

    chore: Create bot project
    Author: Felix Hake

---
+++ src/Bot/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+var app = builder.Build();
+
+await app.StartAsync().ConfigureAwait(false);
+await app.WaitForShutdownAsync().ConfigureAwait(false);
