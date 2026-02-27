f4f07f9be45141eff5063d1353d716780746d7c3
SaveApis | Felix Hake <150133464+saveapisfelixhake@users.noreply.github.com>
2026-02-27 21:33:09 +0100

    feat: Add tailwindcss (#11)
    Author: Felix Hake

--- src/Frontend/src/main.tsx
+++ src/Frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import './ui/assets/css/main.css'
+import './ui/assets/css/tailwindcss.css'
+
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import AppView from './ui/views/AppView.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <AppView/>
+    </StrictMode>,
 )
