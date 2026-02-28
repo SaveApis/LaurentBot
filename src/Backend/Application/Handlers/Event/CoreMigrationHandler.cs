63806b5497c2032baff0ca113807735c78a7f22f
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 21:53:53 +0100

    feat: Add CoreDbContext and migration handler
    Author: Felix Hake

---
+++ src/Backend/Application/Handlers/Event/CoreMigrationHandler.cs
@@ -0,0 +1,8 @@
+﻿using Backend.Persistence.Sql.Context;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Utils.EntityFramework.Infrastructure.Handlers.Event;
+
+namespace Backend.Application.Handlers.Event;
+
+public class CoreMigrationHandler(ILogger<CoreMigrationHandler> logger, IDbContextFactory<CoreDbContext> factory, IMediator mediator) : BaseMigrationHandler<CoreMigrationHandler, CoreDbContext>(logger, factory, mediator);
---
+++ src/Backend/Persistence/Sql/Context/CoreDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Backend.Domains.Role.Domain.Models.Entities;
+using Backend.Domains.Role.Persistence.Sql.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Utils.EntityFramework.Infrastructure.Persistence.Sql.Context;
+
+namespace Backend.Persistence.Sql.Context;
+
+public class CoreDbContext(DbContextOptions<CoreDbContext> options) : BaseDbContext<CoreDbContext>(options)
+{
+    protected override string Schema => "core";
+
+    public DbSet<RoleEntity> Roles => Set<RoleEntity>();
+
+    protected override void ApplyConfigurations(ModelBuilder modelBuilder)
+    {
+        base.ApplyConfigurations(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new RoleEntityTypeConfiguration());
+    }
+}
