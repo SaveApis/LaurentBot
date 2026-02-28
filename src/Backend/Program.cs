9070e98806bd0e0b0b1492f0b6215ec36acf0d13
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 21:57:17 +0100

    feat: Add DI-Module to register roles & services
    Author: Felix Hake

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -1,6 +1,9 @@
 using System.Reflection;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using Backend.Domains.Role.Application.DI;
+using Backend.Persistence.Sql.Context;
+using Utils.EntityFramework.Application.Extensions;
 using Utils.Mediator.Application.DI;
 using Utils.Mediator.Application.Extensions;
 using Utils.Serilog.Application.DI;
@@ -12,6 +15,8 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
         {
             containerBuilder.RegisterModule(new SerilogModule("Backend"));
             containerBuilder.RegisterModule(new MediatorModule(Assembly.GetExecutingAssembly()));
+
+            containerBuilder.RegisterModule(new RoleModule());
         }
     );
 builder.Services.RegisterDbContext<CoreDbContext>("backend");
