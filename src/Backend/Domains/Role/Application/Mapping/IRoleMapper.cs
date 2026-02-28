f749a969ae2d13bc048c132b34feba626dd400d4
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 21:55:32 +0100

    feat: Create mapping class
    Author: Felix Hake

---
+++ src/Backend/Domains/Role/Application/Mapping/IRoleMapper.cs
@@ -0,0 +1,17 @@
+﻿using Backend.Domains.Role.Application.Models.DTO;
+using Backend.Domains.Role.Application.Models.HTTP.Responses;
+using Backend.Domains.Role.Domain.Models.Entities;
+using Backend.Domains.Role.Infrastructure.Roles;
+
+namespace Backend.Domains.Role.Application.Mapping;
+
+public interface IRoleMapper
+{
+    IEnumerable<RoleGetDto> ToDto(IEnumerable<RoleEntity> entities);
+    RoleGetDto ToDto(RoleEntity entity);
+    RoleCreateDto ToCreateDto(IRole role);
+    RoleUpdateDto ToUpdateDto(IRole role);
+
+    IEnumerable<RoleResponse> ToResponse(IEnumerable<RoleGetDto> dtos);
+    RoleResponse ToResponse(RoleGetDto dto);
+}
