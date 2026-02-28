8dbbed183dc29146e78bdfafc6f6f42686b24199
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 21:55:19 +0100

    feat: Create dto and http models
    Author: Felix Hake

---
+++ src/Backend/Domains/Role/Application/Models/DTO/RoleCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Domains.Role.Application.Models.DTO;
+
+public class RoleCreateDto
+{
+    public required string Key { get; init; }
+    public required string Name { get; init; }
+    public required string Description { get; init; }
+}
