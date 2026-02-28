7c4437464a015a32d3bf7a4e5abdba203c0c7ba3
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 17:35:09 +0100

    feat: Add entity & migration infrastructure
    Author: Felix Hake

---
+++ src/libraries/Utils/EntityFramework/Application/Events/MigrationCompletedEvent.cs
@@ -0,0 +1,8 @@
+﻿using Utils.Mediator.Infrastructure.Events;
+
+namespace Utils.EntityFramework.Application.Events;
+
+public class MigrationCompletedEvent(Type contextType) : IEvent
+{
+    public Type ContextType { get; } = contextType;
+}
---
+++ src/libraries/Utils/EntityFramework/Infrastructure/Persistence/Sql/Configurations/BaseTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Utils.EntityFramework.Infrastructure.Persistence.Sql.Entities;
+
+namespace Utils.EntityFramework.Infrastructure.Persistence.Sql.Configurations;
+
+public abstract class BaseTypeConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : class, IEntity
+{
+    protected abstract string TableName { get; }
+
+    protected abstract void ConfigureProperties(EntityTypeBuilder<TEntity> builder);
+    protected abstract void ConfigureIndexes(EntityTypeBuilder<TEntity> builder);
+    protected abstract void ConfigureRelationships(EntityTypeBuilder<TEntity> builder);
+
+    public void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.ToTable(TableName.ToLowerInvariant().Replace(' ', '_'));
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("id").IsRequired();
+        builder.Property(x => x.CreatedAt).HasColumnName("created_at").IsRequired();
+        builder.Property(x => x.UpdatedAt).HasColumnName("updated_at").IsRequired();
+        builder.Property(x => x.Version).HasColumnName("version").IsRequired();
+        ConfigureProperties(builder);
+        ConfigureIndexes(builder);
+        ConfigureRelationships(builder);
+    }
+}
