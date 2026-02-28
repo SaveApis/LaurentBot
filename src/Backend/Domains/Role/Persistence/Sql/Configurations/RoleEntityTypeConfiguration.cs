7b090ada6654d12465a4a0d6ac7ac41675db2c30
SaveApis | Felix Hake <felix.hake@saveapis.com>
2026-02-28 21:54:18 +0100

    feat: Create role entity & migrations
    Author: Felix Hake

---
+++ src/Backend/Domains/Role/Persistence/Sql/Configurations/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Backend.Domains.Role.Domain.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Utils.EntityFramework.Infrastructure.Persistence.Sql.Configurations;
+
+namespace Backend.Domains.Role.Persistence.Sql.Configurations;
+
+public class RoleEntityTypeConfiguration : BaseTypeConfiguration<RoleEntity>
+{
+    protected override string TableName => "roles";
+
+    protected override void ConfigureProperties(EntityTypeBuilder<RoleEntity> builder)
+    {
+        builder.Property(x => x.Key).IsRequired().HasColumnName("key").HasMaxLength(50);
+        builder.Property(x => x.Name).IsRequired().HasColumnName("name").HasMaxLength(100);
+        builder.Property(x => x.Description).IsRequired().HasColumnName("description").HasMaxLength(500);
+    }
+
+    protected override void ConfigureIndexes(EntityTypeBuilder<RoleEntity> builder)
+    {
+        builder.HasIndex(x => x.Key).IsUnique();
+    }
+}
